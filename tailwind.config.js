9750eca07c700ec7028726ac9e51b4d8e4b494e6
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-02-17 11:14:10 +0530

    ui: basic setup of tailwind config, vars for color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./*.hbs",
-    '**/*.hbs',
-  ],
+  content: ["./*.hbs", "**/*.hbs"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      primary: "hsl(var(--color-primary) / 0.8)",
+      secondary: "hsl(var(--color-secondary) / 0.8)",
+      transparent: "transparent",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        sm: ".3rem",
+        md: ".5rem",
+        lg: "1rem",
+        xl: "1.5rem",
+      },
+    },
   },
   plugins: [],
-}
+};
